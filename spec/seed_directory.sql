f20dbcb4b48e7b6cc80a434320ff70dac0e80723
awdem <wdemontmollin@gmail.com>
2023-04-27 14:32:33 +0100

    wrote test seed

---
+++ spec/seed_directory.sql
@@ -0,0 +1,19 @@
+
+
+TRUNCATE TABLE students, cohort RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+
+INSERT INTO cohorts (cohort_name, starting_date) VALUES
+('April', '2023-03-13'),
+('January', '2023-01-13'),
+('October', '2023-10-13');
+
+INSERT INTO students (name, cohort_id) VALUES 
+('David', 1),
+('Emma', 1),
+('John', 2),
+('Carrie', 2),
+ ('Anna', 3);
