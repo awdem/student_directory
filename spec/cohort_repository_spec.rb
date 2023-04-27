8c78e676183898b812c6cb57511a3118341e6eea
awdem <wdemontmollin@gmail.com>
2023-04-27 14:40:17 +0100

    set up cohort repo spec file

---
+++ spec/cohort_repository_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'cohort_repository'
+
+def reset_test_tables
+  seed_sql = File.read('spec/seeds_directory.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'student_directory_2_test' })
+  connection.exec(seed_sql)
+end
+
+
+RSpec.describe CohortRepository do
+  before(:each) do 
+    reset_test_tables
+  end  
+
+  
+end
